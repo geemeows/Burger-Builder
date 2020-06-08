4b1ddac9bbcafd0ba3a40fa5a94411b43a1e0b86
Gazouly <m.algazouly96@gmail.com>
2020-06-09 00:59:46 +0200

    Some performance improvements

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,21 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Backdrop from '@/components/Backdrop/Backdrop'
 
 import classes from './Modal.module.css'
-const Modal = ({ children, show, closeSummary }) => {
-    return (
-        <React.Fragment>
-            <Backdrop show={show} dismiss={closeSummary}/>
-            <div 
-                className={classes.Modal}
-                style={{
-                    transform: show ? 'translateY(0)' : 'translateY(-100vh)',
-                    opacity: show ? '1' : '0'
-                }}> 
-                {children}
-            </div>
-        </React.Fragment>
-    )
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show
+    }
+    componentDidUpdate() {
+        console.log('[Modal.js] ModalDidUpdate')
+    }
+    render () {
+        const { children, show, closeSummary } = this.props
+        return (
+            <React.Fragment>
+                <Backdrop show={show} dismiss={closeSummary}/>
+                <div 
+                    className={classes.Modal}
+                    style={{
+                        transform: show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: show ? '1' : '0'
+                    }}> 
+                    {children}
+                </div>
+            </React.Fragment>
+        )
+    }
 }
 
 export default Modal
--- src/components/OrderSummary/OrderSummary.js
+++ src/components/OrderSummary/OrderSummary.js
@@ -1,26 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Button from '@/components/Button/Button'
-const OrderSummary = ({ ingredients, cancelBehavior, continueBehavior, price }) => {
-    const ingredientsList = Object.keys(ingredients)
+class OrderSummary extends Component {
+
+    componentDidUpdate() {
+        console.log('[OrderSummary] OrderSummaryDidUpdate!!')
+    }
+    render () {
+        const { ingredients, cancelBehavior, continueBehavior, price } = this.props
+        const ingredientsList = Object.keys(ingredients)
         .map(ingredient => (
             <li key={ingredient}>
                 <span style={{ textTransform: 'capitalize'}}>{ingredient}</span>: {ingredients[ingredient]}
             </li>
         ))
-    return (
-        <React.Fragment>
-            <h3>Your Order Summary</h3>
-            <p>A delicious burger with the following ingredients</p>
-            <ul>
-                {ingredientsList}
-            </ul>
-            <p>Your subtotal: <strong>{price.toFixed(2)}</strong></p>
-            <p>Continue to checkout?</p>
-            <Button btnType="Danger" behavior={cancelBehavior}>CANCEL</Button>
-            <Button btnType="Success" behavior={continueBehavior}>CONTINUE</Button>
-        </React.Fragment>
-    )
+        return (
+            <React.Fragment>
+                <h3>Your Order Summary</h3>
+                <p>A delicious burger with the following ingredients</p>
+                <ul>
+                    {ingredientsList}
+                </ul>
+                <p>Your subtotal: <strong>{price.toFixed(2)}</strong></p>
+                <p>Continue to checkout?</p>
+                <Button btnType="Danger" behavior={cancelBehavior}>CANCEL</Button>
+                <Button btnType="Success" behavior={continueBehavior}>CONTINUE</Button>
+            </React.Fragment>
+        )
+    }
 }
 
 export default OrderSummary
